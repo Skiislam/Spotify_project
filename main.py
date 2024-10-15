000ce0e43e6904598663c3cc53d16e5299e2a401
skiislam <sisla016@ucr.edu>
2024-10-15 16:15:14 -0700

    Fixed Oauth using Spotipy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, request, url_for, session
-import spotipy import spotify
-from spotipy import oauth2
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
 import time
 import os
 
@@ -10,45 +11,43 @@ app.secret_key = os.urandom(24)
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = 'http://127.0.0.1:5000/redirect'
+REDIRECT_URI = 'http://localhost:5000/callback'
+scope = 'user-top-read'
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
 
+cache_handler = FlaskSessionCacheHandler(session)
 
-def create_spotify_auth():
-    return oauth2(
-        client_id = CLIENT_ID,
-        client_secret = CLIENT_SECRET,
-        redirect_uri = url_for('redirect'), _external = True,
-        scope = 'user-library-read'
-    )
+spOauth = SpotifyOAuth(
+    client_id = CLIENT_ID,
+    client_secret = CLIENT_SECRET,
+    redirect_uri = REDIRECT_URI,
+    scope = scope,
+    cache_handler = cache_handler,
+    show_dialog = True
+)
 
-@app.route('/login')
-def login():
-    auth_url = create_spotify_auth.get_authorize_url()
-    return redirect(auth_url)
 
-@app.route('/redirect')
-def redirect_page():
-    session.clear()
-    code = request.args.get('code')
-    token_info = create_spotify.auth().get_access_token(code)
-    session[TOKEN_INFO] = token_info
-    return redirect(url_for('recentListen', external = True))
+sp = Spotify(auth_manager = spOauth)
 
-@app.route('/recentListen')
+@app.route('/')
+def home():
+    if not spOauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = spOauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_user_top_songs'))
 
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        redirect(url_for('login', external = False))
-    now = int(time.time())
-    is_expired = token_info['expires_at'] - now < 60
-    if(is_expired):
-        spotify_auth = create_spotify_auth()
-        token_info = spotify_auth.refresh_access_token(token_info['refresh_token'])
-
-    return token_info
+@app.route('/callback')
+def callback():
+    spOauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_top_songs'))
+@app.route('/get_top_songs')
+def get_top_songs():
+    if not spOauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = spOauth.get_authorize_url()
+        return redirect(auth_url)
+    top_track = sp.current_user_top_tracks(limit = 10, offset = 0, time_range = 'medium_term')
+    top_info = []
 
 app.run(debug=True)
